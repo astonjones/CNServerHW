a7170218a6247f94e1e29dcfca0c0109f43d459a
Aston Jones <astonbradleyjones@gmail.com>
2021-12-03 18:09:34 -0600

    App complete and ready for turn in.

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-const express = express();
+const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.send("This is the index route");
 });
 
-app.get('/palindrome', function(req, res){
-    let j = req.body.length -1;
+app.post('/palindrome', function(req, res){
+    let object = req.body;
+    let string = object.value
+    let j = string.length - 1;
+    object.isPalindrome = true;
     for( let i = 0 ; i < j/2 ;i++)
     {
-      let x = str[i] ;//forward character
-      let y = str[j-i];//backward character
-      if( x != y)
+      let x = string[i] ;//forward character
+      let y = string[j-i];//backward character
+      if(x != y)
       {
         // return false if string not match
-        res.status(200).send("That is a palindrome!");
+        object.isPalindrome = false;
       }
     }
-    /// return true if string is palindrome
-    res.status(400).send("That is not a palindrome");
+    if(object.isPalindrome == true){
+        object.isPalindrome = true;
+        console.log(`${object.value} is a palindrome!`)
+        res.status(200).send(object);
+    }
+    else {
+        console.log(`${object.value} is not a palindrome!`)
+        res.status(200).send(object);
+    }
+});
+
+app.listen(3001, () => {
+    console.log('Server listening on port 3001');
 });
